bcba73c6a2a33a092dba6fe2fbc215828df65e21
Tim Sutton <tim@linfiniti.com>
2014-07-22 16:01:06 +0200

    Fix credential fixing

--- deploy.sh
+++ deploy.sh
@@ -1,63 +1,30 @@
 #!/bin/bash
 
+echo ""
+echo "----------------------------------------"
 echo "This script will deploy InaSAFE realtime"
 echo "in a series of docker containers"
 echo "----------------------------------------"
+echo ""
 
+function kill_container {
+    NAME=$1
 
-function deploy_btsync_server {
-    echo "Setting up btsync server"
-    echo "Building SFTP Server image"
-    DATADIR=/home/realtime/analysis_data
-    BTSYNC_IMAGE=docker-realtime-btsync
-
-    docker.io kill ${BTSYNC_IMAGE}
-    docker.io rm ${BTSYNC_IMAGE}
-
-    docker.io build -t ${ORG}/${BTSYNC_IMAGE} git://github.com/${ORG}/${BTSYNC_IMAGE}.git
-
-    mkdir -p ${DATADIR}
-
-    docker.io run --name="${BTSYNC_IMAGE}" \
-        -v $DATADIR:$DATADIR \
-        -p 8888:8888 \
-        -p 55555:55555 \
-        -d -t AIFDR/${BTSYNC_IMAGE}
+    if docker.io ps -a | grep ${NAME} > /dev/null
+    then
+        echo "Killing ${NAME}"
+        docker.io kill ${NAME}
+        docker.io rm ${NAME}
+    else
+        echo "${NAME} is not already running"
+    fi
 }
 
-
-function deploy_sftp_server {
-    echo "Building SFTP Server image"
-    sudo mkdir ${REALTIME_DIR}
-    sudo chown ${USER}.${USER} ${REALTIME_DIR}
-    pushd .
-    cd ${REALTIME_DIR}
-    SFTP_IMAGE=docker-realtime-sftp
-    docker.io build -t ${ORG}/${SFTP_IMAGE} git://github.com/${ORG}/${SFTP_IMAGE}.git
-
-    echo "Starting SFTP Server image"
-    docker.io kill ${SFTP_IMAGE}
-    docker.io rm ${SFTP_IMAGE}
-
-    mkdir -p ${SHAKEDIR}
-    docker.io run --name="${SFTP_IMAGE}" \
-        -v ${SHAKEDIR}:/shakemaps \
-        -p 9222:22 \
-        -d -t ${ORG}/${SFTP_IMAGE}
-}
-
-
-function build_realtime_image {
-    echo "Building InaSAFE Realtime"
-    cd ${REALTIME_DIR}
-
-    echo "Building InaSAFE Realtime Dockerfile"
-    INASAFE_REALTIME_IMAGE=docker-realtime-inasafe
-    docker.io build -t ${ORG}/${INASAFE_REALTIME_IMAGE} git://github.com/${ORG}/${INASAFE_REALTIME_IMAGE}.git
-}
 
 function deploy_apache_server {
+    echo ""
     echo "Building Apache Server"
+    echo "====================================="
     cd ${REALTIME_DIR}
     APACHE_IMAGE=docker-realtime-apache
     docker.io build -t ${ORG}/${APACHE_IMAGE} git://github.com/${ORG}/${APACHE_IMAGE}.git
@@ -66,10 +33,13 @@ function deploy_apache_server {
     WEBDIR=/home/realtime/web
     APACHE_IMAGE=docker-realtime-apache
 
-    docker.io kill ${APACHE_IMAGE}
-    docker.io rm ${APACHE_IMAGE}
+    kill_container ${APACHE_IMAGE}
 
-    mkdir -p $WEBDIR
+    if [ ! -d ${WEBDIR} ]
+    then
+        mkdir -p ${WEBDIR}
+    fi
+
     cp web/index.html ${WEBDIR}/
     cp -r web/resource ${WEBDIR}/
     # Uncomment to run with a bash prompt for testing
@@ -90,14 +60,76 @@ function deploy_apache_server {
         -d -t ${ORG}/${APACHE_IMAGE}
 }
 
-function show_credentials {
+function deploy_sftp_server {
+    echo ""
+    echo "Building SFTP Server image"
+    echo "====================================="
+    sudo mkdir ${REALTIME_DIR}
+    sudo chown ${USER}.${USER} ${REALTIME_DIR}
+    pushd .
+    cd ${REALTIME_DIR}
+    SFTP_IMAGE=docker-realtime-sftp
+    docker.io build -t ${ORG}/${SFTP_IMAGE} git://github.com/${ORG}/${SFTP_IMAGE}.git
 
+    echo "Starting SFTP Server image"
+    kill_container  ${SFTP_IMAGE}
+
+    if [ ! -d ${SHAKEDIR} ]
+    then
+        mkdir -p ${SHAKEDIR}
+    fi
+
+    docker.io run --name="${SFTP_IMAGE}" \
+        -v ${SHAKEDIR}:/shakemaps \
+        -p 9222:22 \
+        -d -t ${ORG}/${SFTP_IMAGE}
+}
+
+
+function deploy_btsync_server {
+    echo ""
+    echo "Setting up btsync server"
+    echo "====================================="
+    DATADIR=/home/realtime/analysis_data
+    BTSYNC_IMAGE=docker-realtime-btsync
+
+    kill_container ${BTSYNC_IMAGE}
+
+    docker.io build -t ${ORG}/${BTSYNC_IMAGE} git://github.com/${ORG}/${BTSYNC_IMAGE}.git
+
+    if [ ! -d ${DATADIR} ]
+    then
+        mkdir -p ${DATADIR}
+    fi
+
+    docker.io run --name="${BTSYNC_IMAGE}" \
+        -v $DATADIR:$DATADIR \
+        -p 8888:8888 \
+        -p 55555:55555 \
+        -d -t AIFDR/${BTSYNC_IMAGE}
+}
+
+
+function build_realtime_image {
+    echo ""
+    echo "Building InaSAFE Realtime"
+    echo "====================================="
+    cd ${REALTIME_DIR}
+    echo "Building InaSAFE Realtime Dockerfile"
+    INASAFE_REALTIME_IMAGE=docker-realtime-inasafe
+    docker.io build -t ${ORG}/${INASAFE_REALTIME_IMAGE} git://github.com/${ORG}/${INASAFE_REALTIME_IMAGE}.git
+}
+
+
+function show_credentials {
+    echo ""
     echo "You can copy files into SFTP container with these credentials:"
+    echo "====================================="
     # Note you can run this command any time after the container
     # is started and all containers started will have these
     # same credentials so you should be able to safely destroy
     # and recreate this container
-    docker cp inasafe-realtime-sftp:/credentials .
+    docker.io cp docker-realtime-sftp:/credentials .
     cat credentials
     rm credentials
 }
@@ -106,7 +138,7 @@ function show_credentials {
 if test -z "$1"
 then
   echo "Using ${ORG} organsiation repos on github"
-  export ORG=${ORG}
+  export ORG=AIFDR
 else
   echo "Using personal forks: $1"
   export ORG=$1
@@ -120,5 +152,6 @@ SHAKEDIR=/home/realtime/shakemaps
 
 deploy_apache_server
 deploy_sftp_server
+deploy_btsync_server
 build_realtime_image
 show_credentials
