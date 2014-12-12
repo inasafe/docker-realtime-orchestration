1f88bbe0c134e81171e921ac2d494f31c690462c
Tim Sutton <tim@linfiniti.com>
2014-12-12 15:50:43 +0700

    In progress porting to fig - basic run working but with some rendering issues

--- docker-inasafe/start.sh
+++ docker-inasafe/start.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-export EQ_SFTP_USER_NAME=`cat /credentials | cut -d ':' -f 2 | cut -d ' ' -f 2`
-export EQ_SFTP_USER_PASSWORD=`cat /credentials | cut -d ':' -f 3 | cut -d ' ' -f 2`
+export EQ_SFTP_USER_NAME=realtime
+export EQ_SFTP_USER_PASSWORD=jea4Mighaif0
+#export EQ_SFTP_USER_PASSWORD=`cat /credentials | cut -d ':' -f 3 | cut -d ' ' -f 2`
 # Will get set for us by docker link
-export EQ_SFTP_BASE_URL=${SFTP_LOCAL_IP}
+export EQ_SFTP_BASE_URL=${SFTP_PORT_22_TCP_ADDR}
 # Will get set for us by docker link
-export EQ_SFTP_PORT=${SFTP_LOCAL_PORT}
+export EQ_SFTP_PORT=22
 export EQ_SFTP_BASE_PATH=/home/realtime/shakemaps
 
-cd /home/realtime/src/inasafe
+INASAFE_SOURCE_DIR=/home/realtime/src/inasafe
+
+function get_inasafe {
+
+    echo ""
+    echo "Pulling the latest InaSAFE Realtime from Github."
+    echo "================================================"
+
+    if [ ! -d ${INASAFE_SOURCE_DIR} ]
+    then
+        git clone --branch realtime http://github.com/AIFDR/inasafe.git --depth 1 --verbose ${INASAFE_SOURCE_DIR}
+    else
+        cd ${INASAFE_SOURCE_DIR}
+        git pull origin realtime
+        cd -
+    fi
+}
+
+if [ "$1" == "checkout" ]
+then
+    get_inasafe
+    exit
+fi
+
+cd ${INASAFE_SOURCE_DIR}
 . scripts/realtime/make-latest-shakemap.sh
 . scripts/realtime/make-public.sh
---
+++ inasafe-src/README.txt
@@ -0,0 +1,2 @@
+This directory will be populated with inasafe sources.
+Do not add any files other than this readme to git.
