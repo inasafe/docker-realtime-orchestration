65d726f1da7080b4806fcddfa9443267bae902f3
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2017-12-21 04:16:46 +0700

    Update Docker for custom build

--- production/docker/inasafe/build.sh
+++ production/docker/inasafe/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+REPO_NAME=kartoza
 IMAGE_NAME=realtime-orchestration_inasafe
-IMAGE_TAG=latest
-docker build -t kartoza/${IMAGE_NAME} .
-docker tag kartoza/${IMAGE_NAME} kartoza/${IMAGE_NAME}:${IMAGE_TAG}
+TAG_NAME=latest
+
+if [ -z $INASAFE_REALTIME_TAG ]; then
+	INASAFE_REALTIME_TAG=realtime-backport-cherry-pick
+fi
+echo "INASAFE_REALTIME_TAG=${INASAFE_REALTIME_TAG}"
+
+echo "Build: $REPO_NAME/$IMAGE_NAME:$TAG_NAME"
+
+docker build -t ${REPO_NAME}/${IMAGE_NAME} .
+docker tag ${REPO_NAME}/${IMAGE_NAME} ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
+docker push${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
--- production/docker/sftp/build.sh
+++ production/docker/sftp/build.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+REPO_NAME=kartoza
 IMAGE_NAME=realtime-orchestration_sftp
-IMAGE_TAGE=v3.0
-docker build -t kartoza/${IMAGE_NAME} .
-docker tag kartoza/${IMAGE_NAME} kartoza/${IMAGE_NAME}:${IMAGE_TAG}
+TAG_NAME=v3.0
+docker build -t ${REPO_NAME}/${IMAGE_NAME} .
+docker tag ${REPO_NAME}/${IMAGE_NAME} ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
+docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
