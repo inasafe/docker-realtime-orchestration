2d21d0aff8aeb3419548e7ce75094ec25210690b
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2018-01-11 23:20:18 +0700

    Fix bug in build scripts

--- production/docker/inasafe/build.sh
+++ production/docker/inasafe/build.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-if [ -z $REPO_NAME ]; then
+if [ -z "$REPO_NAME" ]; then
 	REPO_NAME=kartoza
 fi
 
-if [ -z $IMAGE_NAME ]; then
+if [ -z "$IMAGE_NAME" ]; then
 	IMAGE_NAME=realtime-orchestration_inasafe
 fi
 
-if [ -z $TAG_NAME ]; then
+if [ -z "$TAG_NAME" ]; then
 	TAG_NAME=latest
 fi
 
-if [ -z $BUILD_ARGS ]; then
+if [ -z "$BUILD_ARGS" ]; then
 	BUILD_ARGS="--pull --no-cache"
 fi
 
 # Build Args Environment
 
-if [ -z $INASAFE_REALTIME_TAG ]; then
+if [ -z "$INASAFE_REALTIME_TAG" ]; then
 	INASAFE_REALTIME_TAG=realtime-backport-cherry-pick
 fi
 
@@ -29,4 +29,4 @@ echo "Build Args: $BUILD_ARGS"
 
 docker build -t ${REPO_NAME}/${IMAGE_NAME} ${BUILD_ARGS} .
 docker tag ${REPO_NAME}/${IMAGE_NAME} ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
-docker push${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
+docker push ${REPO_NAME}/${IMAGE_NAME}:${TAG_NAME}
